93cdfe4871caec3f02fb4a8babff880d0bffcc6c
mudi <mudi.qinchuan@gmail.com>
2021-01-25 11:57:54 -0800

    hw2

--- Homework/hw2/hw2.ml
+++ Homework/hw2/hw2.ml
@@ -23,144 +23,150 @@ let sort ls = List.sort compare ls
 
 (* Part 1: Printing JSON values *)
 (* 1 *)
-let make_silly_json (i: int) : json =
-        let rec make_silly_json_helper (i: int) (xs : json) : json= 
-                if i = 0 then xs
-                else match  xs with 
-                        Array ss ->  make_silly_json_helper (i-1) (Array (Object [("n", Num (float_of_int i));("b", True)] :: ss ))
-                        | _ -> failwith "illegal argument exception"
-        in make_silly_json_helper i (Array []);;
-
+let make_silly_json (i : int) : json =
+  let rec make_silly_json_helper (i : int) (xs : json) : json =
+    if i = 0 then xs
+    else
+      match xs with
+      | Array ss ->
+          make_silly_json_helper (i - 1)
+            (Array (Object [ ("n", Num (float_of_int i)); ("b", True) ] :: ss))
+      | _ -> failwith "illegal argument exception"
+  in
+  make_silly_json_helper i (Array [])
 
 (* Part 2: Processing JSON values *)
 (* Don't forget to write a comment for problems 7 and 20. *)
 (* 2 *)
 let rec concat_with (sep : string) (ss : string list) : string =
-        match ss with 
-                [] -> ""
-              | s :: [] -> s
-              | s :: rest -> s ^ sep ^ (concat_with sep rest)
+  match ss with
+  | [] -> ""
+  | [ s ] -> s
+  | s :: rest -> s ^ sep ^ concat_with sep rest
 
 (* 3 *)
-let quote_string (s : string) : string =
-        "\"" ^ s ^ "\""
+let quote_string (s : string) : string = "\"" ^ s ^ "\""
 
 (* OCaml's string_of_float is not quite RFC compliant due to its tendency
    to output whole numbers with trailing decimal points without a zero.
    But, printf does the job. *)
-let json_string_of_float (f : float) : string =
-  Printf.sprintf "%g" f
+let json_string_of_float (f : float) : string = Printf.sprintf "%g" f
 
 (* 4 *)
-  (*Array [ Object [("n" , 1), ("b", True)] ; Object [("n" , 1), ("b", True)] ]*)
+(*Array [ Object [("n" , 1), ("b", True)] ; Object [("n" , 1), ("b", True)] ]*)
 let rec string_of_json (j : json) : string =
-        let  rec jsArray_to_list  ( ll: json list ) : string list = 
-                match  ll with 
-                        [] -> []
-                        | h :: ll' -> (string_of_json h) :: (jsArray_to_list ll')
-        in
-        let rec jsObject_to_list (ll : (string * json) list)  : string list = 
-                match ll with 
-                        [] -> []
+  let rec jsArray_to_list (ll : json list) : string list =
+    match ll with
+    | [] -> []
+    | h :: ll' -> string_of_json h :: jsArray_to_list ll'
+  in
+  let rec jsObject_to_list (ll : (string * json) list) : string list =
+    match ll with
+    | [] -> []
+    | h :: rest ->
+        let str, js_obj = h in
+        ((quote_string str ^ " : ") ^ string_of_json js_obj)
+        :: jsObject_to_list rest
+  in
 
-                        | h :: rest -> (
-                                let (str, js_obj) = h in 
-                                (("\"" ^ str ^ "\":" )  ^ (string_of_json js_obj)) :: (jsObject_to_list rest )
-        )
-
-                in
-        match j with
-                Num n -> json_string_of_float n
-              | String s -> ("\"" ^s ^ "\"" )
-              | False -> "false"
-              | True -> "true"
-              | Null -> "null"
-              | Array arr ->  "[" ^ (concat_with "," (jsArray_to_list arr)) ^ "]"
-              | Object obj_list ->   "{" ^ (concat_with "," (jsObject_to_list obj_list)) ^ "}"
-(*
- *
- *              | Object obj_list ->  
- *                              ( "[" ^ (
- *                              match obj_list with 
- *                              [] -> ""
- *                             | obj :: [] -> (
- *                                     let (str, js_obj) = obj in 
- *                                     "{" ^ "\"" ^str ^"\""^ ":" ^ (string_of_json js_obj) ^"}"
- *                                     
- *                                )
- *                             | obj :: rest -> ( 
- *                                     let (str, js_obj) = obj in 
- *                                     "{" ^ "\"" ^str ^"\""^ ":" ^ (string_of_json js_obj) ^"},"
- *
- *                         )
- *                              ) ^"]")
- *)
-
+  match j with
+  | Num n -> json_string_of_float n
+  | String s -> quote_string s
+  | False -> "false"
+  | True -> "true"
+  | Null -> "null"
+  | Array arr -> "[" ^ concat_with ", " (jsArray_to_list arr) ^ "]"
+  | Object obj_list -> "{" ^ concat_with ", " (jsObject_to_list obj_list) ^ "}"
 
 (* 5 *)
-let rec take (n: int) (l: 'a list) : 'a list =
-  failwith "Need to implement: take"
+let rec take (n : int) (l : 'a list) : 'a list =
+  match n with
+  | 0 -> []
+  | x -> ( match l with [] -> [] | xs :: rest -> xs :: take (n - 1) rest)
 
 (* 6 *)
-let rec firsts (l: ('a * 'b) list) : 'a list =
-  failwith "Need to implement: firsts"
+let rec firsts (l : ('a * 'b) list) : 'a list =
+  match l with
+  | [] -> []
+  | xs :: rest ->
+      let a, b = xs in
+      a :: firsts rest
 
 (* 7 *)
 (* write your comment here *)
 
 (* 8 *)
-let rec assoc (k : 'a) (xs: ('a * 'b) list) : 'b option =
-  failwith "Need to implement: assoc"
+let rec assoc (kba : 'a) (xs : ('a * 'b) list) : 'b option =
+  match xs with
+  | [] -> None
+  | ele :: rest ->
+      let p1, p2 = ele in
+      if p1 = kba then Some p2 else assoc kba rest
 
 (* 9 *)
-let dot (j: json) (f: string) : json option = None
+let dot (j : json) (f : string) : json option = 
+        match j with 
+        Object obj_list -> (assoc f obj_list)
+        | _ -> None
 
 (* 10 *)
-let rec dots (j: json) (fs: string list) : json option =
-  failwith "Need to implement: dots"
+let rec dots (j : json) (fs : string list) : json option =
+        match fs with
+                [] -> None
+        | h :: [] -> dot j h
+        | h :: rest -> (
+                let j_response = dot j h in 
+                match j_response with 
+                       None -> None 
+                        |  Some a  -> (dots a rest)
+        )
 
 (* 11 *)
-let one_fields (j: json) : string list =
+let one_fields (j : json) : string list =
   failwith "Need to implement: one_fields"
 
 (* 12 *)
-let no_repeats (xs: 'a list) : bool =
-  failwith "Need to implement: no_repeats"
+let no_repeats (xs : 'a list) : bool = failwith "Need to implement: no_repeats"
 
 (* 13 *)
-let rec recursive_no_field_repeats (j: json) : bool =
+let rec recursive_no_field_repeats (j : json) : bool =
   failwith "Need to implement: recursive_no_field_repeats"
 
 (* 14 *)
-let count_occurrences (xs: 'a list) (e: exn) : (string * int) list =
+let count_occurrences (xs : 'a list) (e : exn) : (string * int) list =
   failwith "Need to implement: count_occurrences"
 
 (* 15 *)
-let rec string_values_for_access_path (fs : string list) (js: json list) 
-: (string list) =
+let rec string_values_for_access_path (fs : string list) (js : json list) :
+    string list =
   failwith "Need to implement: string_values_for_access_path"
 
 (* 16 *)
-let rec filter_access_path_value (fs : string list) (value : string) (js : json list)
-: json list =
+let rec filter_access_path_value (fs : string list) (value : string)
+    (js : json list) : json list =
   failwith "Need to implement: filter_access_path_value"
 
 (* Types for use in problems 17-20. *)
-type rect = { min_latitude: float; max_latitude: float;
-              min_longitude: float; max_longitude: float }
-type point = { latitude: float; longitude: float }
+type rect = {
+  min_latitude : float;
+  max_latitude : float;
+  min_longitude : float;
+  max_longitude : float;
+}
+
+type point = { latitude : float; longitude : float }
 
 (* 17 *)
-let in_rect (r: rect) (p: point) : bool =
+let in_rect (r : rect) (p : point) : bool =
   failwith "Need to implement: in_rect"
 
 (* 18 *)
-let point_of_json (j: json) : point option =
+let point_of_json (j : json) : point option =
   failwith "Need to implement: point_of_json"
 
 (* 19 *)
-let rec filter_access_path_in_rect (fs : string list) (r: rect) (js: json list)
-: json list =
+let rec filter_access_path_in_rect (fs : string list) (r : rect)
+    (js : json list) : json list =
   failwith "Need to implement: filter_access_path_in_rect"
 
 (* Part 3: Analyzing the data *)
@@ -171,10 +177,11 @@ exception SortIsBroken
 
 (* The definition of the U district for purposes of this assignment :) *)
 let u_district =
-  { min_latitude = 47.648637;
+  {
+    min_latitude = 47.648637;
     min_longitude = -122.322099;
     max_latitude = 47.661176;
-    max_longitude = -122.301019
+    max_longitude = -122.301019;
   }
 
 (* Creates a histogram for the given list of strings. 
@@ -184,27 +191,30 @@ let u_district =
 let histogram (xs : string list) : (string * int) list =
   let sorted_xs = List.sort (fun a b -> compare a b) xs in
   let counts = count_occurrences sorted_xs SortIsBroken in
-  let compare_counts ((s1 : string), (n1 : int)) ((s2 : string), (n2 : int)) : int =
+  let compare_counts ((s1 : string), (n1 : int)) ((s2 : string), (n2 : int)) :
+      int =
     if n1 = n2 then compare s1 s2 else compare n1 n2
   in
   List.rev (List.sort compare_counts counts)
 
-let histogram_for_access_path (fs: string list) (js: json list) : (string * int) list =
+let histogram_for_access_path (fs : string list) (js : json list) :
+    (string * int) list =
   histogram (string_values_for_access_path fs js)
 
 (* 20 *)
 let complete_bus_positions_list =
-  match (dot complete_bus_positions "entity") with
-  | Some (Array l) -> l
-  | _ -> []
+  match dot complete_bus_positions "entity" with Some (Array l) -> l | _ -> []
 
 exception Unimplemented
-let route_histogram     = Unimplemented
-let top_three_routes    = Unimplemented
-let buses_in_ud         = Unimplemented
-let ud_route_histogram  = Unimplemented
+
+let route_histogram = Unimplemented
+
+let top_three_routes = Unimplemented
+
+let buses_in_ud = Unimplemented
+
+let ud_route_histogram = Unimplemented
+
 let top_three_ud_routes = Unimplemented
-let all_fourty_fours    = Unimplemented
 
-
-
+let all_fourty_fours = Unimplemented
